60690e1ed8d299bc046e625272d3a5534be1dde7
belbel <z.stoynov@gmail.com>
2019-08-09 11:41:07 +0300

    Add files via upload

---
+++ FoodStore/README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on August 9, 2019, 10:11 am.
---
+++ FoodStore/src/FoodStoreBundle/FoodStoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FoodStoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FoodStoreBundle extends Bundle
+{
+}
